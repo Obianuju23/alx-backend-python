f6ebc466bf021ba6ff78876e95b5c08592004dcd
Obianuju23 <obianujunmoh@gmail.com>
2023-10-13 13:03:02 +0100

    Function that returns total execution time for wait_n given  and

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import asyncio
 import time
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Executes tpotal execution time"""
     wait_n = __import__('1-concurrent_coroutines.py').wait_n
 
