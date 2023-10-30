2f9a155ae4b79ffcf3f06f8e4083ddea3543ea36
Obianuju23 <obianujunmoh@gmail.com>
2023-10-30 20:20:22 +0100

    This method should test that GithubOrgClient.org returns the correct value.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,3 +22,12 @@ class TestGithubOrgClient(TestCase):
         spec = GithubOrgClient(org)
         self.assertEqual(spec.org, expected)
         get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
+
+    def test_public_repos_url(self):
+        """A method that test if test public response works"""
+        expected = "www.yes.com"
+        payload = {"repos_url": expected}
+        to_mock = 'client.GithubOrgClient.org'
+        with patch(to_mock, PropertyMock(return_value=payload)):
+            cli = GithubOrgClient("spec")
+            self.assertEqual(cli._public_repos_url, expected)
